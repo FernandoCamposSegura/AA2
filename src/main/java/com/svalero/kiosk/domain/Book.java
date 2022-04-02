89cad0251fa099ccb912b9a7dd749ea254b472a7
FernandoCamposSegura <campossegura19@gmail.com>
2022-04-02 12:28:43 +0200

    Creando clases

---
+++ src/main/java/com/svalero/kiosk/domain/Book.java
@@ -0,0 +1,72 @@
+package com.svalero.kiosk.domain;
+
+import java.util.Objects;
+
+public class Book {
+    private String title;
+    private String author;
+    private String ISBN;
+    private int numPages;
+    private float price;
+
+    public Book(String title, String author, String ISBN, int numPages, float price) {
+        this.title = title;
+        this.author = author;
+        this.ISBN = ISBN;
+        this.numPages = numPages;
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public int getNumPages() {
+        return numPages;
+    }
+
+    public void setNumPages(int numPages) {
+        this.numPages = numPages;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return ISBN.equals(book.ISBN);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(ISBN);
+    }
+}
